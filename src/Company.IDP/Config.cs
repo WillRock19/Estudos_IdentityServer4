e9e7199b50747a1077bb867f62b81e0a8c289014
William Pinto <william.pinto@lambda3.com.br>
2019-02-17 12:37:11 -0300

    Added default classes from IdentityServer QuickStart; add first configurations to IdentityServer

---
+++ src/Company.IDP/Config.cs
@@ -0,0 +1,49 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Company.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers() => new List<TestUser>()
+        {
+            new TestUser
+            {
+                SubjectId = "d860efca-22d9-47td",
+                Username = "Frank",
+                Password = "password",
+                Claims = new List<Claim>()
+                {
+                    new Claim("given_name", "Frank"),
+                    new Claim("family_name", "Castle")
+                }
+            },
+            new TestUser
+            {
+                SubjectId = "b7539694-97e7-4dfe",
+                Username = "Maria",
+                Password = "password",
+                Claims = new List<Claim>()
+                {
+                    new Claim("given_name", "Maria"),
+                    new Claim("family_name", "Castle")
+                }
+            }
+        };
+
+        //IdentityResources => map to scopes that give access to Identity-related information
+        //ApiResources => map to scopes that give access to Api-resources
+        public static IEnumerable<IdentityResource> GetIdentityResources() => new List<IdentityResource>()
+        {
+            //If a client request the OpenId scope, the subjectId is returned
+            new IdentityResources.OpenId(),
+
+            //If a client request the OpenId scope, the given_name and family_name claims will be returned
+            new IdentityResources.Profile()
+        };
+
+        public static IEnumerable<Client> GetClients() => new List<Client>();
+    }
+}
---
+++ src/Company.IDP/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Company.IDP
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //AddIdentityServer => registers IdentityServer services on ASP.NETCore Depedency Injection;
+            //AddDeveloperSigningCredential => add an in memory credentials to use in developping mode;
+            services.AddIdentityServer()
+                   .AddDeveloperSigningCredential()
+                   .AddTestUsers(Config.GetUsers())
+                   .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                   .AddInMemoryClients(Config.GetClients());
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
